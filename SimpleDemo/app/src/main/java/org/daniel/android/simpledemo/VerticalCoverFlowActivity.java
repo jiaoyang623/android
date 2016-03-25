87011de67dc0723be2c1a802e3651130c4cca92b
Jiao Yang <yangjiao623@gmail.com>
2016-03-25 14:59:44 +0800

    add vertical coverflow view demo

---
+++ SimpleDemo/app/src/main/java/org/daniel/android/simpledemo/VerticalCoverFlowActivity.java
@@ -0,0 +1,15 @@
+package org.daniel.android.simpledemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jiaoyang on 3/25/16.
+ */
+public class VerticalCoverFlowActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_vertial_coverflow);
+	}
+}
---
+++ SimpleDemo/app/src/main/java/org/daniel/android/simpledemo/widgets/VerticalCoverFlowView.java
@@ -0,0 +1,27 @@
+package org.daniel.android.simpledemo.widgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by jiaoyang on 3/25/16.
+ */
+public class VerticalCoverFlowView extends ViewGroup {
+	public VerticalCoverFlowView(Context context) {
+		super(context);
+	}
+
+	public VerticalCoverFlowView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public VerticalCoverFlowView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+	}
+}
